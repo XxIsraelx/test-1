ffe01d2e566050f92fdf8a3995b5b909898eaa94
caiogsilva <isratete353@gmail.com>
2023-09-18 22:03:18 -0300

    venv

---
+++ logica/logica.py
@@ -0,0 +1,28 @@
+print("             quiz de geografia")
+print("qual o maior pais da América latina?")
+res1 = "Brasil"
+userRes= str(input(""))
+if userRes.lower().strip() == res1.lower().strip():
+  print("resposta correta")
+else:
+  print("resposta incorreta")
+  
+res2 = "Canadá"
+print("qual o 2 maior pais do mundo?")
+userRes2 = str(input(""))
+if userRes2.lower().strip() == res2.lower().strip():
+  print("resposta correta")
+else:
+  print("resposta incorreta")
+
+res3 = ("EUA") or ("Estados Unidos")
+print("qual a maior economia do mundo?")
+userRes3 = str(input(""))
+if userRes3.lower().strip() == res3.lower().strip():
+  print("resposta correta")
+else:
+  ("resposta incorreta")
+
+res4 = ("Buenos Aires") or ("buenos aires")
+print("qual a capital da Argentina?")
+userRes4 = str(input(""))…
